a2a424214d8758f5b4fe50251b615fe8f07e19e4
Shaban Buja <94648882+banibuja@users.noreply.github.com>
2025-02-15 23:20:22 +0100

    jota

--- app/api/location/route.js
+++ app/api/location/route.js
@@ -1,32 +1,31 @@
 export async function GET() {
-    try {
-      const ACCOUNT_ID = "1127802";
-      const LICENSE_KEY = "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";
-  
-      const response = await fetch(
-        `https://geoip.maxmind.com/geoip/v2.1/country/me?use_https=1`,
-        {
-          headers: {
-            Authorization: `Basic ${Buffer.from(`${ACCOUNT_ID}:${LICENSE_KEY}`).toString("base64")}`,
-          },
-        }
-      );
-  
-      if (!response.ok) {
-        throw new Error(`Error: ${response.status} ${response.statusText}`);
+  try {
+    const ACCOUNT_ID = process.env.NEXT_PUBLIC_ACCOUNT_ID;
+    const LICENSE_KEY = process.env.NEXT_PUBLIC_LICENSE_KEY;
+
+    const response = await fetch(
+      `https://geoip.maxmind.com/geoip/v2.1/country/me?use_https=1`,
+      {
+        headers: {
+          Authorization: `Basic ${Buffer.from(`${ACCOUNT_ID}:${LICENSE_KEY}`).toString("base64")}`,
+        },
       }
-  
-      const data = await response.json();
-      return new Response(JSON.stringify({ country: data.country.iso_code }), {
-        status: 200,
-        headers: { "Content-Type": "application/json" },
-      });
-    } catch (error) {
-      console.error("Gabim gjatë marrjes së lokacionit:", error);
-      return new Response(JSON.stringify({ error: "Nuk mund të marrim vendndodhjen" }), {
-        status: 500,
-        headers: { "Content-Type": "application/json" },
-      });
+    );
+
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status} ${response.statusText}`);
     }
+
+    const data = await response.json();
+    return new Response(JSON.stringify({ country: data.country.iso_code }), {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (error) {
+    console.error("Gabim gjatë marrjes së lokacionit:", error);
+    return new Response(JSON.stringify({ error: "Nuk mund të marrim vendndodhjen" }), {
+      status: 500,
+      headers: { "Content-Type": "application/json" },
+    });
   }
-  
+}
